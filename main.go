fb5cb282866d3b6a09f8c85a50b32094f91d1c1b
martin.haesler <martin.haesler@gmail.com>
2015-06-24 19:57:42 +0100

    Add file main.go

---
+++ main.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"github.com/google/gxui"
+	"github.com/google/gxui/drivers/gl"
+	"github.com/google/gxui/gxfont"
+	"github.com/google/gxui/math"
+	"github.com/google/gxui/themes/light"
+)
+
+func appMain(driver gxui.Driver) {
+	theme := light.CreateTheme(driver)
+
+}
