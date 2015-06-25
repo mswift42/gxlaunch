359e2fe15c01a9953b9f237a8266572b76418c52
martin.haesler <martin.haesler@gmail.com>
2015-06-25 19:53:41 +0100

    Add simple gui with label.

--- main.go
+++ main.go
@@ -4,11 +4,29 @@ import (
 	"github.com/google/gxui"
 	"github.com/google/gxui/drivers/gl"
 	"github.com/google/gxui/gxfont"
-	"github.com/google/gxui/math"
-	"github.com/google/gxui/themes/light"
+	"github.com/google/gxui/themes/dark"
 )
 
 func appMain(driver gxui.Driver) {
-	theme := light.CreateTheme(driver)
+	theme := dark.CreateTheme(driver)
 
+	font, err := driver.CreateFont(gxfont.Default, 50)
+	if err != nil {
+		panic(err)
+	}
+	window := theme.CreateWindow(300, 100, "Launch")
+	window.SetBackgroundBrush(gxui.CreateBrush(gxui.Gray90))
+	label := theme.CreateLabel()
+	label.SetFont(font)
+	label.SetColor(gxui.Gray10)
+	label.SetText("Launch Stuff")
+
+	window.AddChild(label)
+
+	window.OnClose(driver.Terminate)
+
+}
+
+func main() {
+	gl.StartDriver(appMain)
 }
