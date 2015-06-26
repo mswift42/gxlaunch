fe2b8d5ae0a98efda9d63807cb747ea7fe6b1dec
martin.haesler <martin.haesler@gmail.com>
2015-06-26 21:37:12 +0100

    Add test for findCommand

--- search_test.go
+++ search_test.go
@@ -1,1 +1,13 @@
 package main
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestFindCommand(t *testing.T) {
+	assert := assert.New(t)
+	cmd := findCommand("/", "hallo")
+	assert.Equal(cmd.Path, "/usr/bin/find")
+}
