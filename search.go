3d20d0a0a8548928d0d7d4d4486efb7e6e2c1915
martin.haesler <martin.haesler@gmail.com>
2015-06-26 17:19:13 +0100

    Add docstrings to places and searchresult structs.

--- search.go
+++ search.go
@@ -1,11 +1,14 @@
 package main
 
+// Searchresult represents the result of any local search.
 type Searchresult struct {
-	name  string
-	path  string
-	image string
+	name      string
+	path      string
+	thumbnail string
 }
 
+// Places represents the bookmarked locations of a Gnome desktop, e.g
+// Videos, Documents, Music, Home... .
 type Places struct {
 	location string
 }
