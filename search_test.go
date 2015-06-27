4d6e2e68851c2db061c92401a69cc170a14986b0
martin.haesler <martin.haesler@gmail.com>
2015-06-27 09:28:33 +0100

    Add test for locateCommand.

--- search_test.go
+++ search_test.go
@@ -12,3 +12,11 @@ func TestFindCommand(t *testing.T) {
 	assert.Equal(cmd.Path, "/usr/bin/find")
 	assert.Equal(cmd.Args, []string{"find", "/", "*hallo*"})
 }
+
+func TestLocateCommand(t *testing.T) {
+	assert := assert.New(t)
+	cmd := locateCommand("hallo")
+	assert.Equal(cmd.Path, "/usr/bin/locate")
+	assert.Equal(cmd.Args, []string{"locate", "-l", "20",
+		"-b", "-i", "hallo"})
+}
