13d0b0fb50aeaed40acb71879e15e419a853931f
martin.haesler <martin.haesler@gmail.com>
2015-06-30 20:45:35 +0100

    Add TextBox

--- main.go
+++ main.go
@@ -1,25 +1,25 @@
 package main
 
 import (
-	"fmt"
-
 	"github.com/google/gxui"
 	"github.com/google/gxui/drivers/gl"
 	"github.com/google/gxui/themes/dark"
-	"github.com/mswift42/gxlaunch/search"
 )
 
 func appMain(driver gxui.Driver) {
 	theme := dark.CreateTheme(driver)
 
-	window := theme.CreateWindow(300, 100, "Launch")
+	window := theme.CreateWindow(500, 200, "Launch")
 	window.SetBackgroundBrush(gxui.CreateBrush(gxui.Gray90))
 
+	searchBox := theme.CreateTextBox()
+	searchBox.SetDesiredWidth(400)
+
+	window.AddChild(searchBox)
 	window.OnClose(driver.Terminate)
 
 }
 
 func main() {
 	gl.StartDriver(appMain)
-	fmt.Println(search.Search("living"))
 }
