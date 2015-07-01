0b9b324c62fa549cefa5a34e96ce5d53e4a5ca0f
martin.haesler <martin.haesler@gmail.com>
2015-07-01 17:59:15 +0100

    Set window Background to gray10.

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"github.com/google/gxui"
 	"github.com/google/gxui/drivers/gl"
+	"github.com/google/gxui/math"
 	"github.com/google/gxui/themes/dark"
 )
 
@@ -10,10 +11,14 @@ func appMain(driver gxui.Driver) {
 	theme := dark.CreateTheme(driver)
 
 	window := theme.CreateWindow(500, 200, "Launch")
-	window.SetBackgroundBrush(gxui.CreateBrush(gxui.Gray90))
+	window.SetBackgroundBrush(gxui.CreateBrush(gxui.Gray10))
 
 	searchBox := theme.CreateTextBox()
-	searchBox.SetDesiredWidth(400)
+	searchBox.SetDesiredWidth(500)
+	searchBox.SetMargin(math.Spacing{L: 4, T: 2, R: 4, B: 2})
+
+	searchBox.OnKeyDown(func(ev gxui.KeyboardEvent) {
+	})
 
 	window.AddChild(searchBox)
 	window.OnClose(driver.Terminate)
